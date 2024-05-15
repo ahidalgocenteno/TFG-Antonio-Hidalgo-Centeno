fix acc calculation

utils/train_utils.py
@@ -69,7 +69,10 @@ def train(model, device, train_loader, validation_loader, epochs):
       validation_loss.append(running_loss/len(validation_loader))
       validation_acc.append(correct/total)
 
-  return train_loss, train_acc, validation_loss, validation_acc
+      # total accuracy
+      acc_total = correct/total
+
+  return train_loss, train_acc, validation_loss, validation_acc, acc_total
 
 # Función de pérdida Siamesa
 class ContrastiveLoss(nn.Module):
@@ -100,7 +103,6 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
       model.train()
       # keep track of the running loss
       running_loss = 0.
-      correct, total = 0, 0
 
       for i, (data_1, data_2, target) in enumerate(train_loader, 0):
 
@@ -129,7 +131,6 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
       # evaluate on validation data
       model.eval()
       running_loss = 0.
-      correct, total = 0, 0
 
       for i, (data_1, data_2, target) in enumerate(val_loader, 0):
         # getting the validation set
@@ -143,3 +144,41 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
       validation_loss.append(running_loss/len(val_loader))
 
   return train_loss, validation_loss
+
+# get the accuracy of the siamese with kNN classifier
+def get_accuracy_siamese(model, device, val_loader, class_samples_loader):
+  model.eval()
+  with torch.no_grad():
+    # get the embeddings for the validation set
+    val_embeddings = []
+    val_labels = []
+    for data, target in val_loader:
+      data, target = data.to(device), target.to(device)
+      output = model.get_embedding(data)
+      val_embeddings.append(output)
+      val_labels.append(target)
+    val_embeddings = torch.cat(val_embeddings)
+    val_labels = torch.cat(val_labels)
+
+    # get the embeddings for the class samples
+    class_samples_embeddings = []
+    class_samples_labels = []
+    for data, target in class_samples_loader:
+      data, target = data.to(device), target.to(device)
+      output = model.get_embedding(data)
+      class_samples_embeddings.append(output)
+      class_samples_labels.append(target)
+    class_samples_embeddings = torch.cat(class_samples_embeddings)
+    class_samples_labels = torch.cat(class_samples_labels)
+
+    # get the accuracy
+    correct = 0
+    total = 0
+    for i in range(len(val_embeddings)):
+      distances = torch.norm(val_embeddings[i] - class_samples_embeddings, dim=1)
+      _, predicted = torch.min(distances, 0)
+      if class_samples_labels[predicted] == val_labels[i]:
+        correct += 1
+      total += 1
+
+    return correct/total