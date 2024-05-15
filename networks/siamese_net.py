23479f52972ea6abeb1fa50aca03362c560541c6
Antonio Hidalgo <a.hidalgo@ua.es>
2024-05-15 22:53:44 +0200

    fix dimension mismatch

--- networks/siamese_net.py
+++ networks/siamese_net.py
@@ -65,7 +65,8 @@ class siamese_convolutional_net(nn.Module):
     self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0)
     self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0)
     self.conv6 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0)
-    self.fc1 = nn.Linear(in_features=512, out_features=100)
+    
+    self.fc1 = nn.Linear(in_features=2048, out_features=100)
 
     self.batchnorm1 = nn.BatchNorm2d(num_features=8)
     self.batchnorm2 = nn.BatchNorm2d(num_features=16)
@@ -117,6 +118,8 @@ class siamese_convolutional_net(nn.Module):
       x = torch.flatten(x, 1)
 
       # Fully connected layer 1.
+      x = torch.flatten(x, 1)
+      x = self.dropout(x)
       x = self.fc1(x)
 
       return x
