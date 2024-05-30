e57b6f969ce28cad906cd93912d4b415d2f554bb
Antonio Hidalgo <a.hidalgo@ua.es>
2024-05-30 18:45:41 +0200

    test siamese with features with trained siamese

--- utils/train_test_utils.py
+++ utils/train_test_utils.py
@@ -199,65 +199,12 @@ def test_siamese_network(model, device, test_loader_singles, class_samples_loade
 
   return correct/total
 
-def train_siamese_with_features(model, device, train_loader_pairs, val_loader_pairs, epochs):
-  criterion =  ContrastiveLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-
-  train_loss, validation_loss = [], []
-
-  with tqdm(range(epochs), unit='epoch') as tepochs:
-    tepochs.set_description('Training')
-    for epoch in tepochs:
-      model.train()
-      # keep track of the running loss
-      running_loss = 0.
-
-      for i, (data_1, data_2, target, _, _) in enumerate(train_loader_pairs, 0):
-
-        # getting the training set
-        data_1, data_2, target = data_1.to(device), data_2.to(device), target.to(device)
-
-        # Get the model output (call the model with the data from this batch)
-        salida_1, salida_2 = model(data_1, data_2)
-
-        # Zero the gradients out)
-        optimizer.zero_grad()
-        # Get the Loss
-        loss  = criterion(salida_1, salida_2, target)
-        # Calculate the gradients
-        loss.backward()
-        # Update the weights (using the training step of the optimizer)
-        optimizer.step()
-
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss  # add the loss for this batch
-
-      # append the loss for this epoch
-      train_loss.append(running_loss.detach().cpu().item()/len(train_loader_pairs))
-
-      # evaluate on validation data
-      model.eval()
-      running_loss = 0.
-
-      for i, (data_1, data_2, target, _, _) in enumerate(val_loader_pairs, 0):
-        # getting the validation set
-        data_1,data_2, target = data_1.to(device),data_2.to(device), target.to(device)
-        optimizer.zero_grad()
-        output_1,output_2 = model(data_1, data_2)
-        loss = criterion(output_1, output_2, target)
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss.item()
-
-      validation_loss.append(running_loss/len(val_loader_pairs))
-
-  return train_loss, validation_loss
-
 def test_siamese_with_features(model, device, test_loader_singles, class_samples_loader):
   model.eval()
   with torch.no_grad():
     # get the embeddings for the validation set
-    val_embeddings = []
-    val_labels = []
+    test_embeddings = []
+    test_labels = []
     for data, target, features in test_loader_singles:
       # get data embeddings from siaemse network
       data, target = data.to(device), target.to(device)
@@ -265,10 +212,10 @@ def test_siamese_with_features(model, device, test_loader_singles, class_samples
       # append features to the output
       output = torch.cat((output, features), dim=1)
       # append to the list
-      val_embeddings.append(output)
-      val_labels.append(target)
-    val_embeddings = torch.cat(val_embeddings)
-    val_labels = torch.cat(val_labels)
+      test_embeddings.append(output)
+      test_labels.append(target)
+    test_embeddings = torch.cat(test_embeddings)
+    test_labels = torch.cat(test_labels)
 
     # get the embeddings for the class samples
     class_samples_embeddings = []
@@ -285,13 +232,13 @@ def test_siamese_with_features(model, device, test_loader_singles, class_samples
     # get the accuracy
     correct = 0
     total = 0
-    for i in range(len(val_embeddings)):
+    for i in range(len(test_embeddings)):
       # Repeat val_embeddings[i] to match the shape of class_samples_embeddings
-      val_embedding_repeated = val_embeddings[i].repeat(class_samples_embeddings.shape[0], 1)
+      val_embedding_repeated = test_embeddings[i].repeat(class_samples_embeddings.shape[0], 1)
       # Compute the distances using pairwise_distance
       distances = F.pairwise_distance(val_embedding_repeated, class_samples_embeddings)
       _, predicted = torch.min(distances, 0)
-      if class_samples_labels[predicted] == val_labels[i]:
+      if class_samples_labels[predicted] == test_labels[i]:
         correct += 1
       total += 1
 
