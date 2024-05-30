00dc51a8c51888120d658661d6f1c5807176944b
Antonio Hidalgo <a.hidalgo@ua.es>
2024-05-30 12:00:48 +0200

    merge the testing in train function for siamese

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -14,7 +14,7 @@ def set_device():
 
 def train(model, device, train_loader, validation_loader, epochs):
   criterion =  nn.CrossEntropyLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005) #Nadam, lr= .001
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
   train_loss, validation_loss = [], []
   train_acc, validation_acc = [], []
   with tqdm(range(epochs), unit='epoch') as tepochs:
@@ -94,9 +94,9 @@ class ContrastiveLoss(nn.Module):
 
       return loss_contrastive
 # Entrenamiento Siamesa
-def train_siamese_network(model, device, train_loader,val_loader, epochs):
+def train_siamese_network(model, device, train_loader_pairs,val_loader_pairs, val_loader_singles, class_samples_loader, epochs):
   criterion =  ContrastiveLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005) #Nadam, lr= .001
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005) 
 
   train_loss, validation_loss = [], []
 
@@ -107,7 +107,7 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
       # keep track of the running loss
       running_loss = 0.
 
-      for i, (data_1, data_2, target) in enumerate(train_loader, 0):
+      for i, (data_1, data_2, target) in enumerate(train_loader_pairs, 0):
 
         # getting the training set
         data_1, data_2, target = data_1.to(device), data_2.to(device), target.to(device)
@@ -129,13 +129,13 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
         running_loss += loss  # add the loss for this batch
 
       # append the loss for this epoch
-      train_loss.append(running_loss.detach().cpu().item()/len(train_loader))
+      train_loss.append(running_loss.detach().cpu().item()/len(train_loader_pairs))
 
       # evaluate on validation data
       model.eval()
       running_loss = 0.
 
-      for i, (data_1, data_2, target) in enumerate(val_loader, 0):
+      for i, (data_1, data_2, target) in enumerate(val_loader_pairs, 0):
         # getting the validation set
         data_1,data_2, target = data_1.to(device),data_2.to(device), target.to(device)
         optimizer.zero_grad()
@@ -144,73 +144,15 @@ def train_siamese_network(model, device, train_loader,val_loader, epochs):
         tepochs.set_postfix(loss=loss.item())
         running_loss += loss.item()
 
-      validation_loss.append(running_loss/len(val_loader))
-
-  return train_loss, validation_loss
-
-def train_siamese_with_features(model, device, train_loader, val_loader, epochs):
-  criterion =  ContrastiveLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005) #Nadam, lr= .001
-
-  train_loss, validation_loss = [], []
-
-  with tqdm(range(epochs), unit='epoch') as tepochs:
-    tepochs.set_description('Training')
-    for epoch in tepochs:
-      model.train()
-      # keep track of the running loss
-      running_loss = 0.
-
-      for i, (data_1, data_2, target, features_1, features_2) in enumerate(train_loader, 0):
-
-        # getting the training set
-        data_1, data_2, target = data_1.to(device), data_2.to(device), target.to(device)
-        features_1, features_2 = features_1.to(device), features_2.to(device)
-
-        # Get the model output (call the model with the data from this batch)
-        salida_1, salida_2 = model(data_1, data_2, features_1, features_2)
-
-        # Zero the gradients out)
-        optimizer.zero_grad()
-        # Get the Loss
-        loss  = criterion(salida_1, salida_2, target)
-        # Calculate the gradients
-        loss.backward()
-        # Update the weights (using the training step of the optimizer)
-        optimizer.step()
-
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss  # add the loss for this batch
-
-      # append the loss for this epoch
-      train_loss.append(running_loss.detach().cpu().item()/len(train_loader))
-
-      # evaluate on validation data
-      model.eval()
-      running_loss = 0.
-
-      for i, (data_1, data_2, target, features_1, features_2) in enumerate(val_loader, 0):
-        # getting the validation set
-        data_1,data_2, target = data_1.to(device),data_2.to(device), target.to(device)
-        features_1, features_2 = features_1.to(device), features_2.to(device)
-        optimizer.zero_grad()
-        output_1,output_2 = model(data_1, data_2, features_1, features_2)
-        loss = criterion(output_1, output_2, target)
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss.item()
-
-      validation_loss.append(running_loss/len(val_loader))
-
-  return train_loss, validation_loss
-
-# get the accuracy of the siamese
-def siamese_clasification_kNN(model, device, val_loader, class_samples_loader):
+      validation_loss.append(running_loss/len(val_loader_pairs))
+      
+  # get the accuracy of the siamese with kNN classifier
   model.eval()
   with torch.no_grad():
     # get the embeddings for the validation set
     val_embeddings = []
     val_labels = []
-    for data, target in val_loader:
+    for data, target in val_loader_singles:
       data, target = data.to(device), target.to(device)
       output = model.forward_once(data)
       val_embeddings.append(output)
@@ -242,4 +184,103 @@ def siamese_clasification_kNN(model, device, val_loader, class_samples_loader):
         correct += 1
       total += 1
 
-    return correct/total
+  acc = correct/total
+
+  return train_loss, validation_loss, acc
+
+def train_siamese_with_features(model, device, train_loader_pairs, val_loader_pairs, val_loader_singles, class_samples_loader, epochs):
+  criterion =  ContrastiveLoss()
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+
+  train_loss, validation_loss = [], []
+
+  with tqdm(range(epochs), unit='epoch') as tepochs:
+    tepochs.set_description('Training')
+    for epoch in tepochs:
+      model.train()
+      # keep track of the running loss
+      running_loss = 0.
+
+      for i, (data_1, data_2, target, _, _) in enumerate(train_loader_pairs, 0):
+
+        # getting the training set
+        data_1, data_2, target = data_1.to(device), data_2.to(device), target.to(device)
+
+        # Get the model output (call the model with the data from this batch)
+        salida_1, salida_2 = model(data_1, data_2)
+
+        # Zero the gradients out)
+        optimizer.zero_grad()
+        # Get the Loss
+        loss  = criterion(salida_1, salida_2, target)
+        # Calculate the gradients
+        loss.backward()
+        # Update the weights (using the training step of the optimizer)
+        optimizer.step()
+
+        tepochs.set_postfix(loss=loss.item())
+        running_loss += loss  # add the loss for this batch
+
+      # append the loss for this epoch
+      train_loss.append(running_loss.detach().cpu().item()/len(train_loader_pairs))
+
+      # evaluate on validation data
+      model.eval()
+      running_loss = 0.
+
+      for i, (data_1, data_2, target, _, _) in enumerate(val_loader_pairs, 0):
+        # getting the validation set
+        data_1,data_2, target = data_1.to(device),data_2.to(device), target.to(device)
+        optimizer.zero_grad()
+        output_1,output_2 = model(data_1, data_2)
+        loss = criterion(output_1, output_2, target)
+        tepochs.set_postfix(loss=loss.item())
+        running_loss += loss.item()
+
+      validation_loss.append(running_loss/len(val_loader_pairs))
+
+  model.eval()
+  with torch.no_grad():
+    # get the embeddings for the validation set
+    val_embeddings = []
+    val_labels = []
+    for data, target, features in val_loader_singles:
+      # get data embeddings from siaemse network
+      data, target = data.to(device), target.to(device)
+      output = model.forward_once(data)
+      # append features to the output
+      output = torch.cat((output, features), dim=1)
+      # append to the list
+      val_embeddings.append(output)
+      val_labels.append(target)
+    val_embeddings = torch.cat(val_embeddings)
+    val_labels = torch.cat(val_labels)
+
+    # get the embeddings for the class samples
+    class_samples_embeddings = []
+    class_samples_labels = []
+    for data, target, features in class_samples_loader:
+      data, target = data.to(device), target.to(device)
+      output = model.forward_once(data)
+      output = torch.cat((output, features), dim=1)
+      class_samples_embeddings.append(output)
+      class_samples_labels.append(target)
+    class_samples_embeddings = torch.cat(class_samples_embeddings)
+    class_samples_labels = torch.cat(class_samples_labels)
+
+    # get the accuracy
+    correct = 0
+    total = 0
+    for i in range(len(val_embeddings)):
+      # Repeat val_embeddings[i] to match the shape of class_samples_embeddings
+      val_embedding_repeated = val_embeddings[i].repeat(class_samples_embeddings.shape[0], 1)
+      # Compute the distances using pairwise_distance
+      distances = F.pairwise_distance(val_embedding_repeated, class_samples_embeddings)
+      _, predicted = torch.min(distances, 0)
+      if class_samples_labels[predicted] == val_labels[i]:
+        correct += 1
+      total += 1
+
+  acc = correct/total
+
+  return train_loss, validation_loss, acc
